b67c4fae54e754d9908761d1ebf583206871e3c3
suzu <suzuhonn1004@gmail.com>
2025-07-21 18:52:43 +0900

    ノート一覧,作成,削除機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_21_070258) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_21_080455) do
   create_table "checklist_items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "plan_id"
     t.boolean "is_checked"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+  end
+
+  create_table "notes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "plan_id"
+    t.integer "user_id"
+    t.text "content"
+    t.integer "updated_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "plan_members", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
