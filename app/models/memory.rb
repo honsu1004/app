7d3021595820bd44244c21591d4c2b8407e6be2b
suzu <suzuhonn1004@gmail.com>
2025-07-26 18:14:03 +0900

    URLに文字列も登録できるようにバリデーションを削除

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -4,5 +4,5 @@ class Memory < ApplicationRecord
 
   has_one_attached :media
 
-  validates :url, format: URI::DEFAULT_PARSER.make_regexp(%w[http https]), allow_blank: true
+  # validates :url, format: URI::DEFAULT_PARSER.make_regexp(%w[http https]), allow_blank: true
 end
