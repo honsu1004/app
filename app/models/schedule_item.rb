19576a5c37e26b9de133dc65e4138647a76917ae
suzu <suzuhonn1004@gmail.com>
2025-08-27 00:29:54 +0900

    スケジュールに終了時間を入力しなくても保存できるように修正

--- app/models/schedule_item.rb
+++ app/models/schedule_item.rb
@@ -4,4 +4,14 @@ class ScheduleItem < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  validates :start_time, presence: true
+
+  def formatted_time_range
+    return "" unless start_time.present?
+
+    result = "🕒 #{start_time.strftime('%H:%M')}"
+    result += " 〜 #{end_time.strftime('%H:%M')}" if end_time.present?
+    result
+  end
 end
--- db/migrate/20250817020219_remove_user_id_from_memory_folders.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromMemoryFolders < ActiveRecord::Migration[8.0]
-  def change
-    remove_column :memory_folders, :user_id, :bigint
-  end
-end
