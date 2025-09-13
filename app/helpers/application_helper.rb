ff3c7ec982c635655a7d7cc62b71fdbe79775ca8
suzu <suzuhonn1004@gmail.com>
2025-09-13 17:25:37 +0900

    セキュリティ警告解消

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 module ApplicationHelper
-  def sort_items_by_time(items)
+  def safe_url(url)
+    return "#" if url.blank?
+    
+    begin
+      uri = URI.parse(url)
+      # HTTPまたはHTTPSのURLのみ許可
+      return url if uri.scheme.in?(%w[http https])
+    rescue URI::InvalidURIError
+      # 無効なURLの場合はデフォルトに戻す
+    end
+    
+    "#"
   end
 end
